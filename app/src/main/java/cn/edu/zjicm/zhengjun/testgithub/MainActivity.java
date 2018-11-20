77a54bbe8d7461e2471b5eac971f2f790ecc519b
zhengjun <taurus-zheng@163.com>
2018-11-20 08:40:08 +0800

    修改MainActivity

--- app/src/main/java/cn/edu/zjicm/zhengjun/testgithub/MainActivity.java
+++ app/src/main/java/cn/edu/zjicm/zhengjun/testgithub/MainActivity.java
@@ -3,6 +3,8 @@ package cn.edu.zjicm.zhengjun.testgithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// aaa
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
